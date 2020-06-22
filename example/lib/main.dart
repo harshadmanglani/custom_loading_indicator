b4822bc81b1336535ba73126530b40976a7f9306
Harshad Manglani <harshad.gm@gmail.com>
2020-06-22 22:05:10 +0530

    0.0.2 - stable

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,16 +10,66 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+// CHECK LINES 49 AND 53 FOR THE SAMPLE IMPLEMENTATIONS
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+        routes: {
+          '/CustomCircularExample': (BuildContext context) =>
+              CustomCircularExample(),
+          '/CustomFadeExample': (BuildContext context) => CustomFadeExample()
+        },
+        home: Builder(
+          builder: (context) => Container(
+              child: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/CustomCircularExample');
+                },
+                child: Text("Custom Loading Indicator",
+                    overflow: TextOverflow.clip),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/CustomFadeExample');
+                },
+                child: Text("Custom Fading Indicator",
+                    overflow: TextOverflow.clip),
+              )
+            ],
+          )),
+        ));
+  }
+}
+
+// EXAMPLE - USING THE CUSTOM CIRCULAR LOADING INDICATOR
+class CustomCircularExample extends StatefulWidget {
+  @override
+  _CustomCircularExampleState createState() => _CustomCircularExampleState();
+}
+
+class _CustomCircularExampleState extends State<CustomCircularExample> {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-        child: CustomCircularLoadingIndicator(
-      imagePath: 'images/tooth.png',
-    )
-        //relativeSize is an optional argument, by default it is set to 2 ( 1 <= relativeSize <= 6 )
-        //curveName is an optional argument - an object of the Curves class in Flutter
-        //relativeSpeed is an optional argument, by default it is set to 2 ( 1 <= relativeSpeed <= 6)
-        );
+      child: CustomCircularLoadingIndicator(imagePath: 'images/tooth.png'),
+    );
+  }
+}
+
+// EXAMPLE - USING THE CUSTOM FADE LOADING INDICATOR
+class CustomFadeExample extends StatefulWidget {
+  @override
+  _CustomFadeExampleState createState() => _CustomFadeExampleState();
+}
+
+class _CustomFadeExampleState extends State<CustomFadeExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CustomFadeLoadingIndicator(imagePath: 'images/tooth.png'),
+    );
   }
 }
